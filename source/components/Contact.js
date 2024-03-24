e730a2966b33f862c53f1590e2173e22fec9f45c
Sushmitha_Raj <srajsushmitha@gmail.com>
2024-03-25 11:10:03 +1300

    text change

--- source/components/Contact.js
+++ source/components/Contact.js
@@ -2,7 +2,7 @@ export default Contact = () => {
   return (
     <div >
       <img className="w-3/12 m-auto p-4" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4ciWoIvZjx8EV1IUfeNvTFVwiIuTn_1hiWQ&usqp=CAU"/>
-      <h3 className="w-6/12 m-auto p-4 text-center">Please contact us at : shettyaarav944@gmail.com</h3>
+      <h3 className="w-6/12 m-auto p-4 text-center">Write your queries at : shettyaarav944@gmail.com</h3>
     </div>
   );
 };
